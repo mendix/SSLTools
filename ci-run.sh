6868b405c8b37c6490f6bd18ee66acc71641cd85
Pim van den Berg <pim.van.den.berg@mendix.com>
2017-03-23 16:49:41 +0100

    ci-run.sh: jdk-7u80-oracle-x64 -> jdk-8u102-oracle-x64

--- ci-run.sh
+++ ci-run.sh
@@ -14,7 +14,7 @@ fi
 
 export LL_COMMIT=$(git rev-parse HEAD | cut -c1-8)
 export VERSION="${CI_BUILD_REF_NAME}_${CI_BUILD_ID}_${CI_BUILD_REF:0:8}"
-JAVA_HOME=/usr/lib/jvm/jdk-7u80-oracle-x64/jre mvn package -Dbuild.version="${VERSION}"
+JAVA_HOME=/usr/lib/jvm/jdk-8u102-oracle-x64/jre mvn package -Dbuild.version="${VERSION}"
 
 cp $(pwd)/pom.xml ssltools-${VERSION}.pom
 
